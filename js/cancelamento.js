e9bcab5d6158f75e5b3d350bd4f37e70f108a9d0
Marcelo Junior <marcelojunior.07@live.com>
2022-01-15 08:46:45 -0300

    interface edoc Cancelamento OK

--- js/cancelamento.js
+++ js/cancelamento.js
@@ -1,5 +1,52 @@
-$( ".submit-btn" ).click(function() {
-    var teste = $('input[name="options"]:checked').val();
-    console.log(teste);
-    $(".progress").removeClass("d-none");
-    });
+var urlHm = "https://novonfeqas.avancoinfo.net"
+var urlPrd = "https://www.nerus-edoc.net"
+var url = ""
+
+$(".submit-btn").click(function () {
+
+    let ambiente = $('input[name="options"]:checked').val();
+    if (ambiente == "HM") {
+        url = urlHm;
+    } else {
+        url = urlPrd;
+    }
+    let token = $("#token-input").val();
+    let chave = $("#chave-input").val();
+    let justificativa = $("#justifica-input").val();
+    let storeno = $("#loja-input").val();
+    let body = new Object;
+    body.chave = chave;
+    body.justificativa = justificativa;
+    let header = new Object;
+    header.apikey = token;
+    header.numeroLoja = storeno;
+    $.ajax({
+        url: url + "/api/Nfes/cancelarNfeViaCarga",
+        type: 'post',
+        data: body,
+        headers: header,
+
+        beforeSend: function () {
+            $(".progress").removeClass("d-none");
+            $(".return-object").html("");
+        }
+    })
+        .done(function (data) {
+            $.each(data, function (key, value) {
+                let object = key + ' : ' + value;
+                let row =$("<p>").text(object);
+
+                $(".return-object").append(row);
+            });
+            $(".progress").addClass("d-none");
+
+
+
+        })
+        .fail(function (jqXHR, textStatus, msg) {
+            alert(msg);
+            $(".progress").addClass("d-none");
+        });
+
+});
+
